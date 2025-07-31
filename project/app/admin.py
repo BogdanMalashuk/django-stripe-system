84dd909e46338cd0c15daeeeec3a0440b4bc5944
BogdanMalashuk <kufar.120@gmail.com>
2025-07-31 19:44:33 +0300

    models registered in admin-panel

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,4 +1,32 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Order, OrderItem, Discount, Tax
 
-admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'currency')
+    search_fields = ('name',)
+    list_filter = ('currency',)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+    fields = ('item', 'quantity')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'is_paid', 'total_amount')
+    list_filter = ('is_paid', 'created_at')
+    inlines = [OrderItemInline]
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount')
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount')
