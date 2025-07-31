7bf8976b0b4ff61a65580954a999bbcc57974939
BogdanMalashuk <kufar.120@gmail.com>
2025-08-01 00:16:32 +0300

    webhook, orders urls added

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import item_detail, create_checkout_session
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('items/<int:item_id>/', item_detail, name='item-detail'),
-    path('buy/<int:item_id>/', create_checkout_session, name='buy-item'),
+    path('item/<int:item_id>/', item_detail, name='item-detail'),
+    path('buy/<int:item_id>/', checkout_session, name='buy-item'),
+    path('orders/<int:order_id>/', order_detail, name='order-detail'),
+    path('payment/<int:order_id>/', create_payment, name='payment'),
+    path("webhook/", stripe_webhook, name="stripe-webhook"),
 ]
